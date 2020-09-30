91421de16c5c0ae63f639c54f6bb2ddf5d9e7b9b
kd <eidetic@outlook.com>
2020-09-30 01:02:33 +0100

    test design template

--- auth/main_test.go
+++ auth/main_test.go
@@ -13,10 +13,23 @@ func TestMain(m *testing.M) {
 	a.InitDB()
 	a.StartGRPC()
 	a.InitHandler()
-
 	code := m.Run()
 	clearDB()
 	os.Exit(code)
+}
+
+func TestAddNewUserGRPC()  {
+	// Create golang user object
+	// Call Auth server GRPC Method passing it as parameter
+	// Test for all response possibility
+}
+
+func TestAddNewUserHTTP()  {
+	// Create json byte of user
+	// Pass as http body to route
+	// Marshal to app user golang object
+	// Pass as parameter to handler
+	// Test for all response possibility
 }
 
 func clearDB() {
