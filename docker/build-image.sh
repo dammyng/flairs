8bd9cbaea355d2e2c11f1a4529927ac65cba781e
kd <eidetic@outlook.com>
2020-10-01 06:51:18 +0100

    docker setup

---
+++ docker/build-image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    liumapp.com@gmail.com                          $'
+echo '$  homePage: http://www.liumapp.com                         $'
+echo '$  Github:   https://github.com/liumapp                     $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+docker pull mysql:8.0.21
+
+docker pull rabbitmq:3-management
+
+docker pull redis:6.0.8
+
