f25d6fdb51fc067f43bef70889ca40f0b3da4fa1
kd <eidetic@outlook.com>
2020-11-03 06:11:34 +0100

    Transactions proto

--- party/proto-gen.sh
+++ party/proto-gen.sh
@@ -11,4 +11,10 @@ protoc --proto_path=wallet/api/proto/v1 --proto_path=party --go_out=plugins=grpc
 protoc --proto_path=wallet/api/proto/v1 --proto_path=party --grpc-gateway_out=logtostderr=true:wallet/pkg/api/v1 flairs-wallet.proto
 protoc --proto_path=wallet/api/proto/v1 --proto_path=party --swagger_out=logtostderr=true:wallet/api/swagger/v1 flairs-wallet.proto
 
+
+protoc --proto_path=transaction/api/proto/v1 --proto_path=party --go_out=plugins=grpc:transaction/pkg/api/v1 flairs-transaction.proto
+protoc --proto_path=transaction/api/proto/v1 --proto_path=party --grpc-gateway_out=logtostderr=true:transaction/pkg/api/v1 flairs-transaction.proto
+protoc --proto_path=transaction/api/proto/v1 --proto_path=party --swagger_out=logtostderr=true:transaction/api/swagger/v1 flairs-transaction.proto
+
+
 $SHELL
